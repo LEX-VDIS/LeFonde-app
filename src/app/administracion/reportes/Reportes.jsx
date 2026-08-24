ef93d340780e776cc1f89fe9db606aa134296383
ElTlacuache <irving.delamora@outlook.com>
2026-08-24 00:09:48 -0600

    feat: refactor Reportes component styles and structure; update orden-title class for better layout

--- src/app/administracion/reportes/Reportes.jsx
+++ src/app/administracion/reportes/Reportes.jsx
@@ -26,7 +26,7 @@ export default function Reportes() {
     <div className="app-body">
       <header className="form-header">
         <span className="form-header-span">
-          <span>
+          <span className="orden-title">
             <span className="icon material-symbols-rounded">query_stats</span>
             <label style={{ whiteSpace: "nowrap" }}>Reporte de ventas</label>
             <select id="serv" name="servicio">
@@ -41,95 +41,93 @@ export default function Reportes() {
             <label>{new Date().toLocaleDateString()}</label>
           </span>
         </span>
-        <div className="form-body">
-          <div className="abd-left">
-            <span className="orden-title">
-              <span className="icon material-symbols-rounded">analytics</span>
-              <span>Resumen</span>
-            </span>
-            <div className="detallecard-container-cards">
-              <div className="infoCard">
-                <div className="colIcon">
-                  <span className="icon material-symbols-rounded infoCard-icon">
-                    room_service
-                  </span>
-                </div>
-                <div className="colText">
-                  <label className="key"> Total de ordenes</label>
-                  <label className="value"> {ordenes.length}</label>
-                </div>
+      </header>
+      <div className="form-body">
+        <div className="abd-left">
+          <span className="orden-title">
+            <span className="icon material-symbols-rounded">analytics</span>
+            <span>Resumen</span>
+          </span>
+          <div className="detallecard-container-cards">
+            <div className="infoCard">
+              <div className="colIcon">
+                <span className="icon material-symbols-rounded infoCard-icon">
+                  room_service
+                </span>
               </div>
-              <div className="infoCard">
-                <div className="colIcon">
-                  <span className="icon material-symbols-rounded infoCard-icon">
-                    money_bag
-                  </span>
-                </div>
-                <div className="colText">
-                  <label className="key"> Total de ganancia</label>
-                  <label className="value">
-                    {" "}
-                    $
-                    {ordenes.reduce(
-                      (acc, orden) => acc + parseInt(orden.total),
-                      0,
-                    )}
-                  </label>
-                </div>
+              <div className="colText">
+                <label className="key"> Total de ordenes</label>
+                <label className="value"> {ordenes.length}</label>
               </div>
             </div>
-            <div className="form-footer">
-              <div className="form-footer-left"></div>
-              <div className="form-footer-right">
-                <button className="accion seccion blue" type="submit">
-                  <span className="icon material-symbols-rounded">save</span>
-                  Exportar
-                </button>
+            <div className="infoCard">
+              <div className="colIcon">
+                <span className="icon material-symbols-rounded infoCard-icon">
+                  money_bag
+                </span>
+              </div>
+              <div className="colText">
+                <label className="key"> Total de ganancia</label>
+                <label className="value">
+                  {" "}
+                  $
+                  {ordenes.reduce(
+                    (acc, orden) => acc + parseInt(orden.total),
+                    0,
+                  )}
+                </label>
               </div>
             </div>
           </div>
-          <div className="abd-right">
-            <span className="orden-title">
-              <span className="icon material-symbols-rounded">
-                room_service
-              </span>
-              <label style={{ whiteSpace: "nowrap" }}>
-                Ordenes del {new Date().toLocaleDateString()}
-              </label>
-            </span>
-            <div className="detallecard-container">
-              <table className="ordenes-table">
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Fecha</th>
-                    <th>Hora</th>
-                    <th>Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {ordenes &&
-                    ordenes.map((orden, index) => (
-                      <tr key={index}>
-                        <td>{orden.idorden}</td>
-                        <td>{new Date(orden.fecha).toLocaleDateString()}</td>
-                        <td>
-                          {new Date(
-                            `1970-01-01T${orden.hora}Z`,
-                          ).toLocaleTimeString([], {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}
-                        </td>
-                        <td>${orden.total}</td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
+          <div className="form-footer">
+            <div className="form-footer-left"></div>
+            <div className="form-footer-right">
+              <button className="accion seccion blue" type="submit">
+                <span className="icon material-symbols-rounded">save</span>
+                Exportar
+              </button>
             </div>
           </div>
         </div>
-      </header>
+        <div className="abd-right">
+          <span className="orden-title">
+            <span className="icon material-symbols-rounded">room_service</span>
+            <label style={{ whiteSpace: "nowrap" }}>
+              Ordenes del {new Date().toLocaleDateString()}
+            </label>
+          </span>
+          <div className="detallecard-container">
+            <table className="ordenes-table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Fecha</th>
+                  <th>Hora</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {ordenes &&
+                  ordenes.map((orden, index) => (
+                    <tr key={index}>
+                      <td>{orden.idorden}</td>
+                      <td>{new Date(orden.fecha).toLocaleDateString()}</td>
+                      <td>
+                        {new Date(
+                          `1970-01-01T${orden.hora}Z`,
+                        ).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </td>
+                      <td>${orden.total}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
